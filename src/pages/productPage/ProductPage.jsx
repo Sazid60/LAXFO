3f68e0fea38338ef2d47ebdf7ba3361885ce17b0
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-02 18:20:19 +0600

    Add:Red Banner Above Footer Added

--- src/pages/productPage/ProductPage.jsx
+++ src/pages/productPage/ProductPage.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import RedBanner from '../../components/footer/RedBanner';
 
 const ProductPage = () => {
     return (
         <div>
             <h1>This is Product Page</h1>
+            <RedBanner />
         </div>
     );
 };
