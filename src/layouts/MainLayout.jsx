d21287faa13e25c9e1a69b384b50e9e8c27e4a8e
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-02 15:08:13 +0600

    Add:Qoute Above Navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,8 +4,13 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
-        <div>
-            <Outlet />
+        <div >
+        <div className='font-libre text-center text-[10px] lg:text-xs bg-[#DCD2BB] py-2'>
+            <h1>Our new website is live! While we fine-tune, your patience is appreciated</h1>
+        </div>
+            <div>
+                <Outlet />
+            </div>
         </div>
     );
 };
